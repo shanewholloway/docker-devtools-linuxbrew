674711f4138f7400a57c82e87da7b1e4ac28a24a
Shane Holloway <shane.holloway@ieee.org>
2016-05-05 20:29:01 -0600

    First working version

---
+++ build-all.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+echo ":: Building 'devtools-linuxbrew' chain" \
+  \
+  && echo "\n\n:: Build 'devtools-linuxbrew:base' \n" \
+  && docker build \
+      -t shane/devtools-linuxbrew:base-xenial \
+      -t shane/devtools-linuxbrew:base \
+      ./devtools-base \
+  \
+  && echo "\n\n:: Build 'devtools-linuxbrew:base' \n" \
+  && docker build \
+      -t shane/devtools-linuxbrew:base-xenial \
+      -t shane/devtools-linuxbrew:base \
+      ./devtools-core \
+  \
+  && echo "\n\n:: Build 'devtools-linuxbrew:tip' \n" \
+  && docker build \
+      -t shane/devtools-linuxbrew:tip-xenial \
+      -t shane/devtools-linuxbrew:tip \
+      -t shane/devtools-linuxbrew:latest \
+      ./devtools-tip \
+  \
+  && echo "\n\n:: Completed building 'devtools-linuxbrew' chain \n" \
+  && true
